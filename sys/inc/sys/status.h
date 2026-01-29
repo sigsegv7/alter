fa6fe52c2052907a88a7f61a0903cb9ca212b741
Ian Moffett <ian@osmora.org>
2026-01-29 17:38:43 -0500

    sys: Add sys/status.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ sys/inc/sys/status.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef _SYS_STATUS_H_
+#define _SYS_STATUS_H_ 1
+
+#define STATUS_SUCCESS      0   /* Success */
+#define STATUS_FAILURE      1   /* Failure */
+#define STATUS_NOT_FOUND    2   /* Resource not found */
+#define STATUS_NO_MEM       3   /* Out of memory */
+#define STATUS_DENIED       4   /* Access denied */
+#define STATUS_BUSY         5   /* Resource is busy */
+
+#endif  /* !_SYS_STATUS_H_ */
